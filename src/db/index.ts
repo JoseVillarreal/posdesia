b1e17583abae1600b3e11266b0e7f32033a0955d
Jose Villarreal <josemvillarrealcs@gmail.com>
2025-01-22 04:04:06 -0600

    updates gitignore; adds deno lock file for dependency version tracking, adds database creation functions

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 // Import Schemas
+import { users as usersSchema, poems as poemsSchema, annotations as annotationsSchema, comments as commentsSchema } from "./schema.ts";
 
 import pg from "pg";
 
@@ -8,5 +9,32 @@ export const db = drizzle({
     client: new Pool({
         connectionString: Deno.env.get("DATABASE_URL"),
     }),
-    schema: {},
+    schema: { usersSchema, poemsSchema, annotationsSchema, commentsSchema },
 });
+
+// Create
+
+export async function insertUser(userObj: typeof usersSchema){
+    return await db.insert(usersSchema).values(userObj);
+}
+
+export async function insertPoem(poemObj: typeof poemsSchema){
+    return await db.insert(poemsSchema).values(poemObj);
+}
+
+export async function insertAnnotation(annotationObj: typeof annotationSchema){
+    return await db.insert(annotationSchema).values(annotationObj);
+}
+
+export async function insertComment(commentObj: typeof commentSchema){
+    return await db.insert(commentSchema).values(commentObj);
+}
+
+// Retrieve
+
+// Update
+
+//TODO: add annotation function
+export async function addAnnotation()
+
+// Delete
